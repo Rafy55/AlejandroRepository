6ea999c1243bf989c83b83e68b7980d1b25a8562
Rafy55 <aaguil5@masonlive.gmu.edu>
2019-11-21 18:54:01 -0500

    jkkb

--- Arrays/js/color.js
+++ Arrays/js/color.js
@@ -4,12 +4,13 @@ $(document).ready(function() {
 });
 
 function colorGuess() {
-	var colorArray = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+	var colorArray = ["Mario","DonkeyKong","Link","Samus","DarkSamus","Yoshi","Kirby","Fox","Pikachu","Luigi","Ness","CaptainFalcon","Jigglypuff","Peach","Daisy","Bowser","IceClimbers","Shiek","Zelda","DrMario","Pichu","Falco","Marth","Lucina","YoungLink","Ganondorf","Mewtwo","Roy","Chrom","MrGame&Watch","MetaKnight","Pit","DarkPit","ZeroSuitSamus","Wario","Snake","Ike","PokemonTrainer","DiddyKong","Lucas","Sonic","KingDedede","Olimar","Lucario","ROB","ToonLink","Wolf","Villager","MegaMan","WiiFitTrainer","Rosalina","LittleMac","Greninja","MiiFighter","Palutena","PacMan","Robin","Shulk","BowserJr","DuckHunt","Ryu","Ken","Cloud","Corrin","Bayonetta","Inkling","Ridley","Simon","Richter","KingKRool","Isabelle","Incineroar","PiranhaPlant","Joker","Hero","Banjo&Kazooie","Terry"];
 	var randomNumber = Math.floor(Math.random() * colorArray.length);
 	var stripped = colorArray[randomNumber].toLowerCase();
 	console.log(colorArray[randomNumber]);
 	console.log(stripped);
-	$('body').css('background-color', colorArray[randomNumber]);
+	// $('body').css('background-color', colorArray[randomNumber]);
+	$('body').css(colorArray[randomNumber]);
 	$('#submit').click(function() {
 		var input = $('input').val();
 		var guess = input.toLowerCase();
